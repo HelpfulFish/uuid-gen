3a1efa3031d31b19d95fa5bfc10723b3b4548f85
HelpfulFish <learning_open_source_dev@proton.me>
2024-02-10 14:39:00 +1300

    chore: remove useless comment

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -11,12 +11,12 @@ const handleWithBlockCopy = (withBlockCopy: boolean, ids: string[]): string[] =>
 };
 
 const handleArrayToString = (ids: string[]): string => {
-  if (ids.length === 0) return ""; // handle empty array case
+  if (ids.length === 0) return "";
 
   // Remove ",\n" from the last item
   const lastItem = ids[ids.length - 1].replace(/,\s*$/, "");
 
-  // Join the array elements with a space
+  // Join array elements with a space
   return [...ids.slice(0, -1), lastItem].join(" ");
 };
 
