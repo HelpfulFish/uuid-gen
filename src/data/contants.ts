ca4089888507dae8ecfbf14b54ff087cf23ac7d8
HelpfulFish <learning_open_source_dev@proton.me>
2024-02-17 14:28:21 +1300

    feat: add app version to title

--- src/data/contants.ts
+++ src/data/contants.ts
@@ -1,3 +1,5 @@
-export const TITLE = "UUID-Gen";
+import { VERSION } from "./version";
+
+export const TITLE = `UUID-Gen v${VERSION}`;
 export const LOCAL_STORAGE_WITH_QUOTES = "with-quotes";
 export const LOCAL_STORAGE_WITH_BLOCK_COPY = "with-block-copy";
